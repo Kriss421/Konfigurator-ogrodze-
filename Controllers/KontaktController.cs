60cb629eeb4261efb1b3a79b1544705d0b626fed
Aga <akamiekus@gmail.com>
2023-05-20 09:38:01 +0200

    Aga-przycisk Pomoc i Kontakt

---
+++ Controllers/KontaktController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Konfigurator_ogrodzeń.Controllers
+{
+    public class KontaktController : Controller
+    {
+        public ActionResult StronaKontakt()
+        {
+            return View("Kontakt");
+        }
+    }
+}
---
+++ Controllers/PomocController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Konfigurator_ogrodzeń.Controllers
+{
+    public class PomocController : Controller
+    {
+        public ActionResult StronaPomoc()
+        {
+            return View("Pomoc");
+        }
+    }
+}
